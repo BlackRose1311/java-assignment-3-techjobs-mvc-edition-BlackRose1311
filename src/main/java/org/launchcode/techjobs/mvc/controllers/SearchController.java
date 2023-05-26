56c69f53b6864494c4fad18475b58cab70185baf
BlackRose1311 <mr.powers1311@gmail.com>
2023-05-25 19:05:44 -0500

    SILL FAILING ONE >:(

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -29,7 +29,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all") || searchTerm.isEmpty()) {
+        if (searchType.toLowerCase().equals("all") || searchTerm.equals("")) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
